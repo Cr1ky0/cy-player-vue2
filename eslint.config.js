d73dcb5f8f3c1bcce3d7d94e617c6742abc2299d
Creekyu <xby503094716@gmail.com>
2024-07-12 21:23:55 +0800

    init

---
+++ eslint.config.js
@@ -0,0 +1,30 @@
+const globals = require('globals');
+const pluginVue = require('eslint-plugin-vue');
+const pluginPrettier = require('eslint-plugin-prettier');
+const vueParser = require('vue-eslint-parser');
+
+module.exports = [
+  {
+    languageOptions: {
+      globals: globals.browser,
+      parser: vueParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        parser: '@babel/eslint-parser',
+      },
+    },
+    files: ['**/*.js', '**/*.vue'],
+    ignores: ['**/dist/**', '**/node_modules/**'],
+    // recommended.plugins: ['prettier']
+  },
+  ...pluginVue.configs['flat/essential'],
+  {
+    plugins: {
+      prettier: pluginPrettier,
+    },
+    rules: {
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+];
--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,12 @@
 <template>
-  <div
-      id="cy-player-container"
-      class="cy-player-container">
-    <video
-        class="cy-player"
-        id="cy-player"
-        ref="videoRef"
-        :autoplay="option.autoPlay"
-        :src="option.videoSrc"
-    >
-      <source :src="option.videoSrc" type="video/mp4"/>
-      <source :src="option.videoSrc" type="video/webm"/>
-      <source :src="option.videoSrc" type="video/ogg"/>
-      <source :src="option.videoSrc" type="application/x-mpegURL"/>
-      <source :src="option.videoSrc" type="application/vnd.apple.mpegURL"/>
-    </video>
-  </div>
+  <div id="app"></div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-  components: {}
-}
+  components: {},
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
